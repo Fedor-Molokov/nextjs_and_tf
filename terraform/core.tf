6719f7aad00c888e61df0be4e8fade0baab1af89
fedor.molokov <fedor.molokov@onlinesup.com>
2024-12-05 16:49:38 +0500

    add core lyambdas

--- terraform/core.tf
+++ terraform/core.tf
@@ -21,3 +21,35 @@ resource "aws_s3_bucket" "static_assets" {
     allowed_headers = ["*"]
   }
 }
+
+resource "aws_lambda_function" "server_function" {
+  function_name = "nextjs-server-function"
+  runtime       = "nodejs18.x"
+  handler       = "index.handler"
+  role          = var.lambda_execution_role_arn
+
+  filename         = "../out/server-functions.zip"
+  source_code_hash = filebase64sha256("../out/server-functions.zip")
+
+  environment {
+    variables = {
+      NODE_ENV = "sobes"
+    }
+  }
+}
+
+resource "aws_lambda_function" "image_optimization_function" {
+  function_name = "nextjs-image-optimization"
+  runtime       = "nodejs18.x"
+  handler       = "index.handler"
+  role          = var.lambda_execution_role_arn
+
+  filename         = "../out/image-optimization-function.zip"
+  source_code_hash = filebase64sha256("../out/image-optimization-function.zip")
+
+  environment {
+    variables = {
+      NODE_ENV = "sobes"
+    }
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,7 @@ variable "aws_region" {
 variable "bucket_name" {
   default = "nextjs-static-assets"
 }
+
+variable "lambda_execution_role_arn" {
+  default = "arn:aws:iam::301225589079:role/lambda-sobes-role"
+}
