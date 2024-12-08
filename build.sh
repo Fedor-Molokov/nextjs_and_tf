890fa0444cca1fda252e439f7f2166c25848dce6
fedor.molokov <fedor.molokov@onlinesup.com>
2024-12-09 01:41:00 +0500

    add modules opennext

--- build.sh
+++ build.sh
@@ -2,7 +2,4 @@ docker build -t devops-task .
 docker create --name devops-task devops-task
 docker cp devops-task:/app/.open-next/ ./out
 docker rm devops-task
-zip -r ./out/image-optimization-function.zip ./out/image-optimization-function
-zip -r ./out/revalidation-function.zip ./out/revalidation-function
-zip -r ./out/server-functions.zip ./out/server-functions
-zip -r ./out/warmer-function.zip ./out/warmer-function
+mv ./out/server-functions ./out/server-function
--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "nextjs-static-assets-terraform"
     key            = "state.tfstate"
@@ -11,3 +16,37 @@ terraform {
   }
 }
 
+locals {
+  default_tags = {
+    Project     = "terraform-aws-opennext"
+    Environment = "example"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+module "opennext" {
+  source       = "./modules/opennext"
+
+  prefix              = "opennext-${random_id.suffix.hex}"
+  default_tags        = local.default_tags
+  opennext_build_path = "./out"
+  hosted_zone_id      = ""
+
+  cloudfront = {
+    aliases             = []
+    acm_certificate_arn = ""
+    assets_paths        = []
+  }
+  
+}
